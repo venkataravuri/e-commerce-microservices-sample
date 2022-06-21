7dcf70bd19831087a6dc97e8117e8178d15af016
Venkata SubbaRao Ravuri <vravuri@vravuri-a01.vmware.com>
2022-06-21 16:32:11 +0530

    introduce web-store-front

---
+++ web-store-front/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './Components/App/App';
+import reportWebVitals from './reportWebVitals';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
---
+++ web-store-front/src/theme.js
@@ -0,0 +1,39 @@
+import { red } from '@material-ui/core/colors';
+import { createMuiTheme } from '@material-ui/core/styles';
+// A custom theme for this app
+const theme = createMuiTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#61dafb',
+      light: '#61dafb',
+      dark: '#21a1c4',
+    },
+    secondary: {
+      main: '#b5ecfb',
+      light: '#61dafb',
+      dark: '#21a1c4',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#282c34',
+    },
+  },
+  overrides: {
+    MuiPaper: {
+      root: {
+        padding: '20px 10px',
+        margin: '10px',
+        backgroundColor: '#fff', // 5d737e
+      },
+    },
+    MuiButton: {
+      root: {
+        margin: '5px',
+      },
+    },
+  },
+});
+export default theme;
