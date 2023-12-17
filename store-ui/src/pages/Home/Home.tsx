c8b454af0827a38413c9800766222ea67f6b27af
배대철 <qoeocjf5@gmail.com>
2023-12-17 16:08:45 +0900

    style : 줄넘김 등 변화

--- store-ui/src/pages/Home/Home.tsx
+++ store-ui/src/pages/Home/Home.tsx
@@ -1,42 +1,49 @@
-import data from '../../utils/assets'
-import './Home.css';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import Deals from '../../components/Deals/Deals'
+import data from "../../utils/assets";
+import "./Home.css";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Deals from "../../components/Deals/Deals";
 import { useNavigate } from "react-router-dom";
 
 function Home() {
   const navigate = useNavigate();
-  
+
   return (
     <div>
       <Box sx={{ flexGrow: 1, p: 2 }}>
         <Deals />
       </Box>
       <Grid container>
-        <Grid item sx={{ pl: 2, pr: 2, width: '100%'}}>
+        <Grid item sx={{ pl: 2, pr: 2, width: "100%" }}>
           <Paper elevation={3} sx={{ pl: 2, pb: 2 }}>
-            <Typography variant="h6" sx={{ p: 1, color: 'text.primary' }}>Shop by Category</Typography>
+            <Typography variant="h6" sx={{ p: 1, color: "text.primary" }}>
+              Shop by Category
+            </Typography>
             <Grid container spacing={2}>
               <>
-                {
-                  data['categories'].map((category, index) => (
-                    <Grid item key={index}>
-                      <Card sx={{ width: 160, height: 200 }}>
-                        <CardContent>
-                          <Typography color="text.secondary" >
-                            {category.name}
-                          </Typography>
-                          <Box> <img src={category.image} height="128" alt={category.name}></img></Box>
-                        </CardContent>
-                      </Card>
-                    </Grid>
-                  ))
-                }
+                {data["categories"].map((category, index) => (
+                  <Grid item key={index}>
+                    <Card sx={{ width: 160, height: 200 }}>
+                      <CardContent>
+                        <Typography color="text.secondary">
+                          {category.name}
+                        </Typography>
+                        <Box>
+                          {" "}
+                          <img
+                            src={category.image}
+                            height="128"
+                            alt={category.name}
+                          ></img>
+                        </Box>
+                      </CardContent>
+                    </Card>
+                  </Grid>
+                ))}
               </>
             </Grid>
           </Paper>
