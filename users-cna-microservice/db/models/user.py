a8d097f01e23f60ca7eb0ff7472daf5a68489c31
배대철 <qoeocjf5@gmail.com>
2023-12-18 17:33:30 +0900

    Update: User model, add Pydantic models, declare Base locally, and rename orm_mode to from_attributes - 이 변경 사항은 User 모델을 업데이트하고, Pydantic UserBase, UserIn, UserOut 모델을 추가하며, Base를 로컬로 선언하고, orm_mode를 from_attributes로 변경하는 것을 포함합니다.

--- users-cna-microservice/db/models/user.py
+++ users-cna-microservice/db/models/user.py
@@ -1,11 +1,27 @@
+from pydantic import BaseModel
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
 
-from db.config import Base
-
+Base = declarative_base()
 class User(Base):
-    __tablename__ = 'user'
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    email = Column(String, nullable=False)
-    mobile = Column(Integer, nullable=False)
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    mobile = Column(String, unique=True, index=True)
+
+class UserBase(BaseModel):
+    name: str
+    email: str
+    mobile: int
+
+class UserIn(UserBase):
+    pass
+
+class UserOut(UserBase):
+    id: int
+
+#'orm_mode' has been renamed to 'from_attributes'
+    class Config:
+        from_attributes = True
