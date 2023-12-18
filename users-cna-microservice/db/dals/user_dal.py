107b2107721130866cc5cbcd324073b05ca9ad2b
배대철 <qoeocjf5@gmail.com>
2023-12-18 17:32:32 +0900

    Refactor: UserDAL methods to use Pydantic models and return UserOut - UserDAL은 Data Access Layer의 약자로, 데이터베이스와의 상호작용을 캡슐화하는 클래스입니다. 이 클래스는 데이터베이스에서 데이터를 가져오거나 데이터베이스에 데이터를 저장하는 등의 작업을 수행합니다.
    이 경우, UserDAL은 사용자(User) 데이터에 대한 CRUD(Create, Read, Update, Delete) 작업을 수행하는 메서드를 제공합니다. 이 메서드들은 FastAPI 라우트 핸들러에서 호출되어 사용자 데이터를 처리합니다.
    
    예를 들어, create_user 메서드는 새 사용자를 생성하고 데이터베이스에 저장하는 작업을 수행하며, get_all_users 메서드는 데이터베이스에서 모든 사용자 데이터를 가져오는 작업을 수행합니다. 이와 같은 방식으로 UserDAL 클래스는 데이터베이스 작업을 캡슐화하여 코드의 재사용성과 유지 관리성을 향상시킵니다.

--- users-cna-microservice/db/dals/user_dal.py
+++ users-cna-microservice/db/dals/user_dal.py
@@ -4,32 +4,29 @@ from sqlalchemy import update
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 
-from db.models.user import User
+from db.models.user import User, UserIn, UserOut
 
 class UserDAL():
     def __init__(self, db_session: Session):
         self.db_session = db_session
 
-    async def create_user(self, name: str, email: str,   mobile: str):
-        new_user = User(name=name,email=email, mobile=mobile)
+    async def create_user(self, user: UserIn) -> UserOut:
+        new_user = User(name=user.name, email=user.email, mobile=user.mobile)
         self.db_session.add(new_user)
         await self.db_session.flush()
+        return UserOut.from_orm(new_user)
 
-    async def get_all_users(self) -> List[User]:
+    async def get_all_users(self) -> List[UserOut]:
         q = await self.db_session.execute(select(User).order_by(User.id))
-        return q.scalars().all()
+        return [UserOut.from_orm(user) for user in q.scalars().all()]
 
-    async def get_user(self, user_id: str) -> User:
+    async def get_user(self, user_id: str) -> UserOut:
         q = await self.db_session.execute(select(User).where(User.id == user_id))
-        return q.scalar()
+        return UserOut.from_orm(q.scalar())
 
-    async def update_user(self, user_id: int, name: Optional[str], email: Optional[str], mobile: Optional[str]):
-        q = update(User).where(User.id == user_id)
-        if name:
-            q = q.values(name=name)
-        if email:
-            q = q.values(email=email)
-        if mobile:
-            q = q.values(mobile=mobile)
+    async def update_user(self, user_id: int, user: UserIn) -> UserOut:
+        q = update(User).where(User.id == user_id).values(name=user.name, email=user.email, mobile=user.mobile)
         q.execution_options(synchronize_session="fetch")
-        await  self.db_session.execute(q)
+        await self.db_session.execute(q)
+        updated_user = await self.get_user(user_id)
+        return updated_user
