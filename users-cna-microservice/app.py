171fd038536fb1d593dfe32251e96e740494f6e1
배대철 <qoeocjf5@gmail.com>
2023-12-18 17:27:57 +0900

    Refactor: user routes to use Pydantic models and add HTTPException handling

--- users-cna-microservice/app.py
+++ users-cna-microservice/app.py
@@ -1,31 +1,48 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker
+from db.models.user import User, UserIn, UserOut
+from db.config import async_session, engine, Base
 import uvicorn
-from db.config import engine, Base
-from routers import user_router
-from fastapi import Depends
-from db.config import async_session
-from db.models.user import User
+from routers.user_router import router
 
+# create db session
+async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+# create FastAPI instance
 app = FastAPI()
-app.include_router(user_router.router)
 
+# import routers
+app.include_router(router)
 
-@app.on_event("startup")
-async def startup():
-    # create db tables
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.drop_all)
-        await conn.run_sync(Base.metadata.create_all)    
-    
+# Define a Pydantic model for User input data
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+@app.post("/users/")
+async def create_user(user: UserIn):
+    # Create a new session
     async with async_session() as session:
-        async with session.begin():
-            session.add_all([
-                User(name = 'Peter', email = 'peter@exmaple.com', mobile='298479284'),
-                User(name = 'John', email = 'john@exmaple.com', mobile='998479284'),
-                User(name = 'Jason', email = 'jason@exmaple.com', mobile='928479285')]
-            )
+        # Create a new user instance
+        db_user = User(name=user.name, email=user.email, mobile=user.mobile)
+        # Add the user instance to the session
+        session.add(db_user)
+        # Commit the session to the database
         await session.commit()
-        
+        # Return the created user
+        return db_user
+
+async def create_initial_data():
+    # create initial data
+    async with async_session() as session:
+        session.add_all([
+            User(name='Peter', email='peter@example.com', mobile='298479284'),
+            User(name='John', email='john@example.com', mobile='998479284'),
+            User(name='Jason', email='jason@example.com', mobile='928479285')]
+        )
+        await session.commit()
 
 if __name__ == '__main__':
     uvicorn.run("app:app", port=9090, host='127.0.0.1', reload=True)
