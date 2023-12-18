aaace3515cb9bf1d75826e48c0fa0ae209a81f70
배대철 <qoeocjf5@gmail.com>
2023-12-19 01:01:47 +0900

    refactor: app.py 원복

--- users-cna-microservice/app.py
+++ users-cna-microservice/app.py
@@ -12,37 +12,26 @@ async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession
 
 # create FastAPI instance
 app = FastAPI()
-
 # import routers
 app.include_router(router)
 
-# Define a Pydantic model for User input data
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-@app.post("/users/")
-async def create_user(user: UserIn):
-    # Create a new session
-    async with async_session() as session:
-        # Create a new user instance
-        db_user = User(name=user.name, email=user.email, mobile=user.mobile)
-        # Add the user instance to the session
-        session.add(db_user)
-        # Commit the session to the database
-        await session.commit()
-        # Return the created user
-        return db_user
-
+@app.on_event("startup")
 async def startup():
-    # create initial data
+    # create db tables
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
+        await conn.run_sync(Base.metadata.create_all)    
+    
     async with async_session() as session:
-        session.add_all([
-            User(name='Peter', email='peter@example.com', mobile='298479284'),
-            User(name='John', email='john@example.com', mobile='998479284'),
-            User(name='Jason', email='jason@example.com', mobile='928479285')]
-        )
+        async with session.begin():
+            session.add_all([
+                User(name = 'Peter', email = 'peter@exmaple.com', mobile='298479284'),
+                User(name = 'John', email = 'john@exmaple.com', mobile='998479284'),
+                User(name = 'Jason', email = 'jason@exmaple.com', mobile='928479285')]
+            )
         await session.commit()
+
+
 
 if __name__ == '__main__':
     uvicorn.run("app:app", port=9090, host='127.0.0.1', reload=True)
