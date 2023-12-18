203577acae50462d91cf0258d41ae811a10bf54f
배대철 <qoeocjf5@gmail.com>
2023-12-18 17:34:29 +0900

    Refactor: router functions to use UserIn and UserOut models and add HTTP exceptions for existing email and non-existent user - UserIn과 UserOut 모델을 사용하도록 라우터 함수를 리팩토링하고, 이미 등록된 이메일 또는 존재하지 않는 사용자에 대한 HTTP 예외를 추가하는 것을 포함

--- users-cna-microservice/routers/user_router.py
+++ users-cna-microservice/routers/user_router.py
@@ -1,29 +1,32 @@
 from typing import List, Optional
-
-from fastapi import APIRouter, Depends
-
+from fastapi import APIRouter, Depends, HTTPException
 from db.dals.user_dal import UserDAL
-from db.models.user import User
+from db.models.user import UserIn, UserOut
 from dependencies import get_user_dal
 
 router = APIRouter()
 
+@router.post("/users", response_model=UserOut)
+async def create_user(user: UserIn, user_dal: UserDAL = Depends(get_user_dal)):
+    db_user = await user_dal.get_user_by_email(user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    return await user_dal.create_user(user)
 
-@router.post("/users")
-async def create_user(name: str, email: str, mobile: str, user_dal: UserDAL = Depends(get_user_dal)):
-    print("name: " + name)
-    return await user_dal.create_user(name, email, mobile)
+@router.put("/users/{user_id}", response_model=UserOut)
+async def update_user(user_id: int, user: UserIn, user_dal: UserDAL = Depends(get_user_dal)):
+    db_user = await user_dal.get_user(user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return await user_dal.update_user(user_id, user)
 
-
-@router.put("/users/{user_id}")
-async def update_user(user_id: int, name: Optional[str] = None, email: Optional[str] = None, mobile: Optional[str] = None,
-                      user_dal: UserDAL = Depends(get_user_dal)):
-    return await user_dal.update_user(user_id, name, email, mobile)
-
-@router.get("/users/{user_id}")
+@router.get("/users/{user_id}", response_model=UserOut)
 async def get_user(user_id: int, user_dal: UserDAL = Depends(get_user_dal)):
-    return await user_dal.get_user(user_id)
+    db_user = await user_dal.get_user(user_id)
+    if not db_user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
 
-@router.get("/users")
-async def get_all_users(user_dal: UserDAL = Depends(get_user_dal)) -> List[User]:
+@router.get("/users", response_model=List[UserOut])
+async def get_all_users(user_dal: UserDAL = Depends(get_user_dal)):
     return await user_dal.get_all_users()
