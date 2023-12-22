fae2a152a57e015d270720876960ea6a0dd3445c
배대철 <qoeocjf5@gmail.com>
2023-12-23 01:53:51 +0900

    feat: 장바구니 삭제

--- cart-cna-microservice/src/main/java/com/ecommerce/cart/controller/CartController.java
+++ cart-cna-microservice/src/main/java/com/ecommerce/cart/controller/CartController.java
@@ -8,6 +8,7 @@ import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.core.ReactiveValueOperations;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.DeleteMapping; 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -67,4 +68,14 @@ public class CartController {
             cartOps.set(c.getCustomerId(), c).subscribe();
         }).then();
     }
-}
+
+    @DeleteMapping("/cart/{customerId}/{productId}")
+    Mono<Void> delete(@PathVariable String customerId, @PathVariable String productId) {
+        return cartOps.get(customerId).flatMap(c -> {
+            //중요!! getItemId() is not exist in CartItem class. so we need to add it. (important!!!)
+            c.getItems().removeIf(item -> item.getProductId().equals(productId));
+            return cartOps.set(c.getCustomerId(), c);
+        }).then();
+    }
+  }
+
